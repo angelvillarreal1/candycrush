5d8383688749c7a854c9788342a70edfa1afb378
angelvillarreal1 <league@WorthyMini-2.local>
2017-07-29 11:29:32 -0700

    that was fast
    lel

---
+++ src/candycrush/Candycrush.java
@@ -0,0 +1,13 @@
+package candycrush;
+
+import javax.swing.JOptionPane;
+
+public class Candycrush {
+	public static void main(String[] args) {
+		String name =JOptionPane.showInputDialog("Who do you not like");
+		JOptionPane.showMessageDialog(null,"You have a crush on "+name);
+	
+		
+	}
+
+}
